3223da63a5f963e75ac48cfcae09fda191192159
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-07-04 22:29:42 +0200

    More complete Connection.

--- src/Network/Nats/Connection.hs
+++ src/Network/Nats/Connection.hs
@@ -1,34 +1,60 @@
-{-# LANGUAGE TupleSections #-}
 -- | Abstraction for a connection towards a NATS server. It owns the
 -- networking stuff and performs NATS handshaking necessary.
 module Network.Nats.Connection
     ( Connection
     , makeConnection
-    , waitForShutdown
+    , waitForServerShutdown
     ) where
 
-import Control.Concurrent.Async (Async, async, waitAnyCatchCancel)
-import Control.Exception
-import Control.Monad (void)
+import Control.Concurrent.Async ( Async
+                                , async
+                                , waitAnyCatchCancel
+                                )
+import Control.Concurrent.STM ( TQueue
+                              , atomically
+                              , readTQueue
+                              , writeTQueue
+                              )
+import Control.Exception ( SomeException
+                         , throwIO
+                         , handle
+                         )
+import Control.Monad (void, forever)
 import Control.Monad.IO.Class (liftIO)
 import Data.ByteString (ByteString)
-import Data.Conduit
+import Data.Conduit ( ($$)
+                    , Source
+                    , Sink
+                    , awaitForever
+                    , yield
+                    )
 import Data.Maybe (fromJust)
-import Network.Socket
-import Network.URI
+import Network.Socket (HostName, PortNumber)
+import Network.URI ( URI
+                   , uriAuthority
+                   , uriRegName
+                   , uriPort
+                   )
 
 import qualified Network.Connection as NC
 
+-- | Upstream data from the NATS server to the client.
+type Upstream = TQueue ByteString
+
+-- | Downstream data from the client to the NATS server.
+type Downstream = TQueue ByteString
+
+-- | Record representing an active connection towards the NATS server.
 data Connection = Connection
     { connection :: !NC.Connection
     , fromNet    :: !(Async ())
     , toNet      :: !(Async ())
     }
 
-makeConnection :: URI
-               -> Source IO ByteString
-               -> Sink ByteString IO ()
-               -> IO (Maybe Connection)
+-- | Make a new 'Connection' as specified by the URI. Provide one
+-- 'Upstream' queue with data from the application to the server, and
+-- one 'Downstream' queue with data from the server to the application.
+makeConnection :: URI -> Upstream -> Downstream -> IO (Maybe Connection)
 makeConnection uri fromApp toApp =
     socketError `handle` (Just <$> makeConnection' uri fromApp toApp)
     where
@@ -37,14 +63,7 @@ makeConnection uri fromApp toApp =
         | isConnectionRefused e = return Nothing
         | otherwise             = throwIO e
 
-isConnectionRefused :: SomeException -> Bool
-isConnectionRefused e =
-    show e == "connect: does not exist (Connection refused)"
-
-makeConnection' :: URI
-               -> Source IO ByteString
-               -> Sink ByteString IO () 
-               -> IO Connection
+makeConnection' :: URI -> Upstream -> Downstream -> IO Connection
 makeConnection' uri fromApp toApp = do
     -- Make the connection.
     ctx  <- NC.initConnectionContext
@@ -58,31 +77,53 @@ makeConnection' uri fromApp toApp = do
 
     -- Now start the pipeline threads and return the Connection
     -- record.
-    Connection conn <$> (async $ connectionSource conn $$ toApp)
-                    <*> (async $ fromApp $$ connectionSink conn)
+    Connection conn <$> (async $ connectionSource conn $$ streamSink toApp)
+                    <*> (async $ streamSource fromApp $$ connectionSink conn)
 
+-- | Blocking wait for the server connection to shutdown (perhaps it
+-- never does).
+waitForServerShutdown :: Connection -> IO ()
+waitForServerShutdown conn = do
+    void $ waitAnyCatchCancel [ fromNet conn, toNet conn]
+    NC.connectionClose $ connection conn
+
+-- | Source from a 'NC.Connection' to a 'ByteString'.
 connectionSource :: NC.Connection -> Source IO ByteString
-connectionSource c = go
-    where
-      go = do
-        yield =<< (liftIO $ NC.connectionGetChunk c)
-        go
+connectionSource c =
+    forever $ yield =<< (liftIO $ NC.connectionGetChunk c)
 
+-- | Sink a 'ByteString' to a 'NC.Connection'.
 connectionSink :: NC.Connection -> Sink ByteString IO ()
 connectionSink c = awaitForever $ 
     \chunk -> liftIO $ NC.connectionPut c chunk
 
+-- | Source from an 'Upstream' to a 'ByteString'.
+streamSource :: Upstream -> Source IO ByteString
+streamSource stream =
+    forever $ yield =<< (liftIO $ atomically $ readTQueue stream)
+
+-- | Sink a 'ByteString' to a 'Downstream'.
+streamSink :: Downstream -> Sink ByteString IO ()
+streamSink stream = awaitForever $
+    \chunk -> liftIO $ atomically $ writeTQueue stream chunk
+
+-- | Select the host part from the 'URI'.
 hostFromUri :: URI -> HostName
 hostFromUri = uriRegName . fromJust . uriAuthority
 
+-- | Select the port part from the 'URI'.
 portFromUri :: URI -> PortNumber
 portFromUri = fromIntegral . extractPort . uriPort . fromJust . uriAuthority
                                 
+-- | When selected the port is a string of format ":4222". Skip the colon,
+-- or if the port is missing, give the default port of 4222.
 extractPort :: String -> Int
-extractPort []      = 4222
-extractPort ":"     = 4222
-extractPort (_:str) = read str
+extractPort []        = 4222
+extractPort ":"       = 4222
+extractPort (':':str) = read str
+extractPort _         = error "This is no valid port, ehh?"
 
-waitForShutdown :: Connection -> IO ()
-waitForShutdown conn = 
-    void $ waitAnyCatchCancel [ fromNet conn, toNet conn]
+-- | Awkward, but this is how to check for connection refuse.
+isConnectionRefused :: SomeException -> Bool
+isConnectionRefused e =
+    show e == "connect: does not exist (Connection refused)"
