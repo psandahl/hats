132980bba15b0c1212c424175bb982ad64fc2563
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-07-08 21:30:49 +0200

    Some documentation to Dispatcher.

--- src/Network/Nats/Dispatcher.hs
+++ src/Network/Nats/Dispatcher.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
+-- | The dispatcher is receiving 'Downstream' messages from the NATS
+-- server and dispatches them to their receivers.
 module Network.Nats.Dispatcher
     ( dispatcher
     ) where
@@ -28,12 +30,16 @@ import Network.Nats.Message.Parser (parseMessage)
 
 import qualified Network.Nats.Message.Message as M
 
+-- | The dispatcher pipeline from the 'Downstream', through the message
+-- parser and to the core dispatcher.
 dispatcher :: Downstream -> Upstream -> SubscriberMap -> IO ()
 dispatcher downstream upstream subscriberMap =
     streamSource downstream              =$= 
         conduitParserEither parseMessage $$ 
         messageSink upstream subscriberMap
 
+-- | The message 'Sink'. Forever receive messages, if there are
+-- parser error print those, otherwise just dispatch the message.
 messageSink :: Upstream -> SubscriberMap 
             -> Sink (Either ParseError (PositionRange, M.Message)) IO ()
 messageSink upstream subscriberMap =
@@ -44,8 +50,12 @@ messageSink upstream subscriberMap =
             Left err       -> liftIO $ putStrLn (show err)
 {-# INLINE messageSink #-}
 
+-- | Dispatch on 'M.Message. Handles 'M.Msg' and 'M.Ping'.
 dispatchMessage :: Upstream -> SubscriberMap -> M.Message -> IO ()
 
+-- Receive one 'M.Msg'. Lookup its 'Subscriber' and feed it with the 
+-- message. If no 'Subscriber' is found, the message is silently
+-- discarded.
 dispatchMessage _ subscriberMap (M.Msg topic sid replyTo payload) = do
     let msg = Msg topic replyTo sid payload
     maybe (return ()) (feedSubscriber msg) =<< 
@@ -59,15 +69,21 @@ dispatchMessage upstream _ M.Ping = upstreamMessage upstream M.Pong
 dispatchMessage _ _ _ = return ()
 {-# INLINE dispatchMessage #-}
 
+-- | Feed a 'Subscriber' with a 'Msg'.
 feedSubscriber :: Msg -> Subscriber -> IO ()
+
+-- Queue subscriber. Write the queue.
 feedSubscriber msg (Subscriber queue _) =
     atomically $ writeTQueue queue msg
+
+-- Async subscriber. Fork a handler.
 feedSubscriber msg (AsyncSubscriber action _) =
     void $ forkIO $ asyncTrampoline (action msg)
 {-# INLINE feedSubscriber #-}
 
+-- | Trampoline to handle exceptions from an async handler.
 asyncTrampoline :: IO () -> IO ()
 asyncTrampoline action = allExceptions `handle` action
     where
       allExceptions :: SomeException -> IO ()
-      allExceptions _ = return ()
+      allExceptions e = putStrLn $ "Async handler crash: " ++ show e
