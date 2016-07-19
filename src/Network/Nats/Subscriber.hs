7baa10e136ee3df21e0b98e5b7461cf3d63195f9
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-07-19 22:13:42 +0200

    Documentation of Network.Nats.Subscriber.

--- src/Network/Nats/Subscriber.hs
+++ src/Network/Nats/Subscriber.hs
@@ -1,3 +1,12 @@
+-- |
+-- Module:      Network.Nats.Subscriber
+-- Copyright:   (c) 2016 Patrik Sandahl
+-- License:     MIT
+-- Maintainer:  Patrik Sandahl <patrik.sandahl@gmail.com>
+-- Stability:   experimental
+-- Portability: portable
+--
+-- Data structures and API for handling of subscribers.
 module Network.Nats.Subscriber
     ( SubscriberMap
     , Subscriber (..)
@@ -30,20 +39,31 @@ import Data.HashMap.Strict (HashMap)
 
 import qualified Data.HashMap.Strict as HM
 
+-- | Map from 'Sid' to 'Subscriber'. Wrapped in a 'TVar'.
 type SubscriberMap = TVar (HashMap Sid Subscriber)
 
+-- | Data structure to describe a subscriber. Each subscriber caches
+-- the SUB 'Message' used to define it. Needed when replaying
+-- subscriptions at server reconnects.
 data Subscriber
     = Subscriber !(TQueue Msg) !Message
+    -- ^ A ordinary subscriber, which is just a 'TQueue' of 'Msg's.
     | AsyncSubscriber !(Msg -> IO ()) !Message
+    -- ^ An asynchronous subscriber, with an IO action taking a
+    -- 'Msg'.
 
+-- | A NATS message as received by the user.
 data Msg = Msg !Topic !(Maybe Topic) {-# UNPACK #-} !Sid !Payload
     deriving (Eq, Show)
 
+-- | A subscriber queue, a queue of 'Msg' handled by a 'TQueue'.
 newtype SubQueue = SubQueue (TQueue Msg)
 
+-- | Create a new empty 'SubscriberMap'.
 newSubscriberMap :: IO SubscriberMap
 newSubscriberMap = newTVarIO HM.empty
 
+-- | Add a new subscriber to the 'SubscriberMap'.
 addSubscriber :: SubscriberMap -> Sid -> Message -> IO SubQueue
 addSubscriber subscriberMap sid msg = do
     queue <- newTQueueIO
@@ -52,6 +72,7 @@ addSubscriber subscriberMap sid msg = do
     return $ SubQueue queue
 {-# INLINE addSubscriber #-}
 
+-- | Add a new, asynchronous, subscriber to the 'SubscriberMap'.
 addAsyncSubscriber :: SubscriberMap -> Sid -> Message
                      -> (Msg -> IO ()) -> IO ()
 addAsyncSubscriber subscriberMap sid msg action = do
@@ -59,16 +80,19 @@ addAsyncSubscriber subscriberMap sid msg action = do
     atomically $ modifyTVar subscriberMap $ HM.insert sid sub
 {-# INLINE addAsyncSubscriber #-}
 
+-- | Remove a subscriber.
 removeSubscriber :: SubscriberMap -> Sid -> IO ()
 removeSubscriber subscriberMap sid =
     atomically $ modifyTVar subscriberMap $ HM.delete sid
 {-# INLINE removeSubscriber #-}
 
+-- | Try to lookup a subscriber.
 lookupSubscriber :: SubscriberMap -> Sid -> IO (Maybe Subscriber)
 lookupSubscriber subscriberMap sid =
     HM.lookup sid <$> atomically (readTVar subscriberMap)
 {-# INLINE lookupSubscriber #-}
 
+-- | Enumerate all subscriber SUB 'Message's from the 'SubscriberMap'.
 subscribeMessages :: SubscriberMap -> IO [Message]
 subscribeMessages subscriberMap =
     map extractMessage . HM.elems <$> readTVarIO subscriberMap
